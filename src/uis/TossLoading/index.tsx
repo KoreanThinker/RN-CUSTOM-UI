a36acd3e75041315ad8c7ad9e99864627bffac1e
hyun <coderhyun476@gmail.com>
2020-03-16 00:41:32 +0900

    tossloading anim

--- src/uis/TossLoading/index.tsx
+++ src/uis/TossLoading/index.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, Animated, View, StyleProp, ViewStyle } from 'react-native'
+import { StyleSheet, Animated, View, StyleProp, ViewStyle, Easing, EasingFunction } from 'react-native'
+
+// bad codes
 
 interface TossLoadingProps {
     size?: number,
@@ -7,22 +9,31 @@ interface TossLoadingProps {
     style?: StyleProp<ViewStyle>,
     number?: number,
     color?: string,
-    duration?: number,
+    totalDuration?: number,
     delay?: number,
+    translateY?: number,
+    itemDuration?: number,
+    translateYSpring?: number,
+    easing?: EasingFunction
 }
 
 const TossLoading: React.FC<TossLoadingProps> = (props) => {
 
     const [animation] = useState(new Animated.Value(0))
 
-    const data = [1, 2, 3, 4]
+    const data = []
+    for (let i = 0; i < props.number; i++) {
+        data.push(0)
+    }
 
     const runAnimation = () => {
+        animation.setValue(0)
         Animated.sequence([
             Animated.delay(props.delay),
             Animated.timing(animation, {
                 toValue: 1,
-                duration: props.duration
+                duration: props.totalDuration,
+                easing: props.easing
             })
         ]).start(() => runAnimation())
     }
@@ -31,15 +42,27 @@ const TossLoading: React.FC<TossLoadingProps> = (props) => {
         runAnimation()
     }, [])
 
-    const Ball = (index: number) =>
-        <View style={{
+    const Ball = (index: number) => {
+
+        const start = (props.totalDuration - props.itemDuration) / (props.number - 1) * index
+        const inputRange = [start / props.totalDuration]
+        for (let i = 1; i < 9; i++) {
+            inputRange.push((props.itemDuration * i / 8 + start) / props.totalDuration)
+        }
+        return <Animated.View key={index} style={{
             width: props.size,
             height: props.size,
             backgroundColor: props.color,
             borderRadius: props.size / 2,
-            marginLeft: index == 0 ? 0 : props.spac
+            marginLeft: index == 0 ? 0 : props.spac,
+            translateY: animation.interpolate({
+                inputRange,
+                outputRange: [0, 0, -props.translateY, 0, props.translateY, 0, props.translateYSpring, 0, 0],
+            })
         }}
         />
+    }
+
 
     return (
         <View style={[styles.container, props.style]} >
@@ -51,12 +74,16 @@ const TossLoading: React.FC<TossLoadingProps> = (props) => {
 }
 
 TossLoading.defaultProps = {
-    size: 6,
+    size: 5,
     spac: 6,
     number: 4,
     color: '#0050FF',
-    duration: 2000,
-    delay: 300
+    totalDuration: 2000,
+    delay: 200,
+    translateY: 7,
+    itemDuration: 1600,
+    translateYSpring: 2,
+    easing: Easing.linear
 }
 
 export default TossLoading
@@ -64,6 +91,6 @@ export default TossLoading
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
-        alignItems: 'center'
+        alignItems: 'center',
     }
 })
--- src/uis/TossLoading/testScreen.tsx
+++ src/uis/TossLoading/testScreen.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet, Easing } from 'react-native'
 import TossLoading from '.'
 
 const testScreen = () => {
     return (
-        <View style={styles.container} >
-            <TossLoading />
-        </View>
+        <>
+            <View style={styles.container} >
+                <TossLoading />
+            </View>
+            <View style={styles.container} >
+                <TossLoading
+                    spac={3}
+                    number={30}
+                    size={1}
+                    delay={200}
+                    translateY={20}
+                    color='#000'
+                    totalDuration={6000}
+                    itemDuration={2000} //itemDuration must lessthen totalDuration
+                    translateYSpring={4}
+                    easing={Easing.out(Easing.bounce)}
+                />
+            </View>
+        </>
     )
 }
 const styles = StyleSheet.create({
